7b5c58aeac14a38e330051fb951a80dae3a34734
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-05 13:00:55 +0100

    Adding Filter component to ContactList

--- src/components/organisms/ContactList/ContactList.jsx
+++ src/components/organisms/ContactList/ContactList.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { getContacts } from 'redux/selectors';
 
+import { Filter } from 'components/molecules/Filter/Filter';
 import { ContactListItem } from 'components/molecules/ContactListItem/ContactListItem';
 
 import styles from './ContactList.module.css';
@@ -9,10 +10,13 @@ export const ContactList = () => {
   const contacts = useSelector(getContacts);
 
   return (
-    <ul className={styles.ContactList}>
-      {contacts.map(contact => (
-        <ContactListItem key={contact.id} item={contact} classes={styles} />
-      ))}
-    </ul>
+    <>
+      {contacts.length > 0 && <Filter />}
+      <ul className={styles.ContactList}>
+        {contacts.map(contact => (
+          <ContactListItem key={contact.id} item={contact} classes={styles} />
+        ))}
+      </ul>
+    </>
   );
 };
