aa901447736eb0d71d489fbfcc96176f9c66841d
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-05 12:43:50 +0100

    Refactoring code completed

--- src/components/molecules/ContactListItem/ContactListItem.jsx
+++ src/components/molecules/ContactListItem/ContactListItem.jsx
@@ -1,13 +1,26 @@
-import PropTypes from 'prop-types';
-import { Button } from 'components/atoms/Button/Button';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/slices/contactsSlice';
+
 import { Span } from 'components/atoms/Span/Span';
+import { Button } from 'components/atoms/Button/Button';
+
+import PropTypes from 'prop-types';
 
 export const ContactListItem = ({ item, classes }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(item.id));
+  };
   return (
     <li className={classes.contactItem} id={item.id}>
       <Span className={classes.contactNumber}>{item.name}</Span>
-      <Span className={classes.contactNumber}>{item.phone}</Span>
-      <Button className={classes.contactButton} type="button">
+      <Span className={classes.contactNumber}>{item.number}</Span>
+      <Button
+        className={classes.contactButton}
+        type="button"
+        onClick={handleDelete}
+      >
         Delete
       </Button>
     </li>
