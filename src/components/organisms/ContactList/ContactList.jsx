a59d1747f5fa22521113e06017899d22d353d4ca
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-05 12:45:26 +0100

    Refactoring code completed

--- src/components/organisms/ContactList/ContactList.jsx
+++ src/components/organisms/ContactList/ContactList.jsx
@@ -1,10 +1,17 @@
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
+
 import { ContactListItem } from 'components/molecules/ContactListItem/ContactListItem';
+
+import PropTypes from 'prop-types';
+
 import styles from './ContactList.module.css';
 
-export const ContactList = ({ contacts, onClick }) => {
+export const ContactList = () => {
+  const contacts = useSelector(getContacts);
+
   return (
-    <ul className={styles.ContactList} onClick={onClick}>
+    <ul className={styles.ContactList}>
       {contacts.map(contact => (
         <ContactListItem key={contact.id} item={contact} classes={styles} />
       ))}
