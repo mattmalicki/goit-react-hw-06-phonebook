809673b5cb15b2b533a4e555d3df0d02ede1a582
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-05 13:00:32 +0100

    Removing Filter component from App and deleting unused imports

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,16 +1,10 @@
-import { useSelector } from 'react-redux';
-import { getContacts } from 'redux/selectors';
-
 import { Header } from 'components/atoms/Header/Header';
-import { Filter } from 'components/molecules/Filter/Filter';
 import { ContactForm } from 'components/organisms/ContactForm/ContactForm';
 import { ContactList } from 'components/organisms/ContactList/ContactList';
 
 import styles from './App.module.css';
 
 export const App = () => {
-  const contacts = useSelector(getContacts);
-
   return (
     <div className={styles.appContainer}>
       <Header level={2} className={styles.appHeader}>
@@ -20,7 +14,6 @@ export const App = () => {
       <Header level={2} className={styles.appHeader}>
         Contacts
       </Header>
-      {contacts.length > 0 && <Filter />}
       <ContactList />
     </div>
   );
