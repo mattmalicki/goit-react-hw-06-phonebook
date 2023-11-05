41f820bdf851cb3ccd7dcccb1b9fc6ddfe8d2502
mattmalicki <mateusz.malicki96@gmail.com>
2023-11-05 13:28:38 +0100

    Adding handleFilter and dispatch

--- src/components/molecules/Filter/Filter.jsx
+++ src/components/molecules/Filter/Filter.jsx
@@ -1,19 +1,27 @@
-import PropTypes from 'prop-types';
-import { Header } from 'components/atoms/Header/Header';
+import { useDispatch } from 'react-redux';
+import { changeFilter } from 'redux/slices/filterSlice';
+
 import { Input } from 'components/atoms/Input/Input';
+import { Header } from 'components/atoms/Header/Header';
+
 import styles from './Filter.module.css';
 
-export const Filter = ({ onChange }) => {
+export const Filter = () => {
+  const dispatch = useDispatch();
+
+  const handleFilter = event => {
+    dispatch(changeFilter(event.target.value));
+  };
   return (
     <>
       <Header level={4} className={styles.filterHeader}>
         Find contacts by name:
       </Header>
-      <Input className={styles.filterInput} type="text" onChange={onChange} />
+      <Input
+        className={styles.filterInput}
+        type="text"
+        onChange={handleFilter}
+      />
     </>
   );
-};
-
-Filter.propTypes = {
-  onChange: PropTypes.func,
 };
